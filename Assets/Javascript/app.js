3bbf65c561e903e5addb011338b6227d2b9f345b
Dewi Nurdin <dewinurdin@gmail.com>
2017-12-08 22:51:46 -0600

    added timer

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -1,18 +1,80 @@
-var questionArr = ["What is the Capital city of Indonesia?", "Pteronophobia is the fear of being tickled by?", 
-					"Banana fruits are curved because they grow?", "Heart attacks are more likely to happen on a?",];
-var choicesArr = ["Jakarta", "Bali", "Java", "Seoul"],
-				 ["Hair", "Feather", "Ice", "Nails"],
-				 ["towards the sun", "in tiers", "from the banana heart", "at the tropical place"],
-				 ["Sunday", "Wednesday", "Monday", "Friday"];
-var answerArr = ["Jakarta", "Feather", "towards the sun", "Monday"];
+var gameInfo = {
+	correct: 0,
+	incorrect: 0,
+	timeLeft: 5,
+	countDown: function() {
+					gameInfo.timeLeft--;
+					$("#timeLeft").html(gameInfo.timeLeft);
+
+					if (timeLeft === 0) {
+						alert("Times Up!");
+						stop();
+					}
+				}
+			};
+
+var timer;
+
+function stop(){
+    	clearInterval(gameInfo.timeLeft); // stops the timer
+    }
+
+// Questions, Choices, Answer
+var questions = [{
+			question: "What is the Capital city of Indonesia?",
+			choices: ["Jakarta", "Bali", "Java", "Seoul"],
+			validAnswer: "Jakarta"
+		}, { question: "Pteronophobia is the fear of being tickled by?",
+			choices: ["Hair", "Feather", "Ice", "Nails"],
+			validAnswer: "Feather"
+		}, { question: "Banana fruits are curved because they grow?",
+			choices: ["towards the sun", "in tiers", "from the banana heart", "at the tropical place"],
+			validAnswer: "towards the sun"
+		}, { question: "Heart attacks are more likely to happen on a?",
+			choices: ["Sunday", "Wednesday", "Monday", "Friday"],
+			validAnswer: "Monday"
+		}];
+
+//Start Game, Display Questions and Choices
+
+		function startGame() {
+			for (var i = 0; i < questions.length; i++) {
+			$("#trivia-questions").append("<h3>" + questions[i].question);
+
+			for (var j = 0; j < questions.length; j++) {
+			$("#trivia-questions").append("<button>" + questions[i].choices[j]);
+			}
+		}
+	 // Timer Interval, Count down 1000 = 1 sec
+		timer = setInterval(gameInfo.countDown, 1000)
+	};
+
+	function finish(){
+    	clearInterval(gameInfo.counter); // stops the timer
+    	timer();
+    }
+
+	startGame();
+
+		function endGame() {
+			$("button").on("click", function() {
+				var userGuess = $(this).html();
+
+				if (userGuess === questions[0].validAnswer || questions[1].validAnswer ||
+					question[2].validAnswer || questions[3].validAnswer) {
+					gameInfo.correct++;
+					$(".correct-answer").text(gameInfo.correct);
+				} else {
+					gameInfo.incorrect++;
+					$(".incorrect-answer").text(gameInfo.incorrect);
+				}
+			})
+		};
+		
+	// Submit answers
+	$("#submit").on("click", finish);  // submits answers and finishes the game
+
+		
 
 
-$("#trivia-questions").html(questionArr);
-console.log("#trivia-questions");
-
-$(".button").html(
-	("#choice1", choicesArr[0]);
-	("#choice2", choicesArr[1]);
-	("#choice3", choicesArr[2]);
-	("#choice4", choicesArr[3]);
-		);
+	
